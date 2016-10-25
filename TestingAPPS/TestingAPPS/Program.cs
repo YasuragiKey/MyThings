4501d23dd4b85be07e24de0df9891050428ba5b6
Richardo Jefferson <mashup808@gmail.com>
2016-10-25 09:57:32 -0500

    added "hello people!"

--- TestingAPPS/TestingAPPS/Program.cs
+++ TestingAPPS/TestingAPPS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace TestingAPPS
 {
@@ -11,6 +12,7 @@ namespace TestingAPPS
         static void Main(string[] args)
         {
             //bloop
+            WriteLine("Hello people!");
         }
     }
 }
