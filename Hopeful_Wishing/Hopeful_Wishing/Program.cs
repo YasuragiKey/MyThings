baac2896e37b328746ac3a8c1c289dfd69d4cecc
Barbara <bmattler92@gmail.com>
2016-10-26 11:12:55 -0500

    Huge Revert

--- Hopeful_Wishing/Hopeful_Wishing/Program.cs
+++ Hopeful_Wishing/Hopeful_Wishing/Program.cs
@@ -13,11 +13,6 @@ namespace Hopeful_Wishing
         {
             WriteLine("So excited for next month minus 6 days! xD");
             WriteLine("What are you excited?");
-            WriteLine("The LAN Party and The Preorder release of Pokemon Sun and Moon! Woot Woot!");
-            WriteLine("The LAN Party is gonna be so much fun!!!!!");
-            WriteLine("Heck yeah it is! best part is they happen on the same day XD");
-            WriteLine("A bunch of games are coming out in the next" +
-                "few days that are gonna flood the LAN Party");
         }
     }
 }
